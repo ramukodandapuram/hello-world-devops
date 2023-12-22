bfada19e5f0b35099de035ff09a333b49e8494ec
ramukodandapuram <ramukodandapuram@gmail.com>
2023-12-22 15:27:43 +0530

    flake8 and test PEP8

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello world!!!"
+    return 'Hello world!!!'
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
+
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
